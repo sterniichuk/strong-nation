1954b2c2dd3b01645ac54d8150374fd2e12198b7
stern <sterniichuk.serhii@gmail.com>
2023-02-19 23:04:36 +0200

    v 0.0.3

--- src/main/java/online/strongnation/business/config/SecurityConstants.java
+++ src/main/java/online/strongnation/business/config/SecurityConstants.java
@@ -2,5 +2,5 @@ package online.strongnation.business.config;
 
 public interface SecurityConstants {
     String LOCAL_HOST_WITH_ENABLED_CROSS_ORIGIN_REQUESTS = "http://localhost:3000";
-    String FRONTEND_WITH_ENABLED_CROSS_ORIGIN_REQUESTS = "https://smerch88.github.io/StrongNationReact";
+    String FRONTEND_WITH_ENABLED_CROSS_ORIGIN_REQUESTS = "https://smerch88.github.io";
 }
